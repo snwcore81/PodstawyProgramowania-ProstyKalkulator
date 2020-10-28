73148093c06a08248e822f501d2cc03f59966c76
Jacek Kuźmicz <snwcore@gmail.com>
2020-10-28 09:24:24 +0100

    pierwsza funkcja

--- Program.cs
+++ Program.cs
@@ -4,35 +4,49 @@ namespace ProstyKalkulator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void MenuOperacji()
         {
-            float _fWynik = 0f;
-            float _fLiczba = 0f;
-            int _iOpcja = -1;
+            Console.WriteLine("0 - Wyjście z programu");
+            Console.WriteLine("1 - Dodawanie");
+            Console.WriteLine("2 - Odejmowanie");
+            Console.WriteLine("3 - Mnożenie");
+            Console.WriteLine("4 - Dzielenie");
+            Console.Write("Wybierz jedna z opcji:");
+        }
 
+        static float PodajLiczbeFloat()
+        {
+            float _fLiczba = 0f;
 
             while (true)
             {
                 Console.Write("Podaj liczbę:");
 
-                if (float.TryParse(Console.ReadLine(), out _fWynik) == true)
+                if (float.TryParse(Console.ReadLine(), out _fLiczba) == true)
                 {
                     break;
                 }
                 Console.WriteLine("Co piszesz gamoniu!?");
             }
 
+            return _fLiczba;
+        }
+
+        static void Main(string[] args)
+        {
+            // typ y = typ f ( x,y )
+
+            float _fWynik = 0f;
+            float _fLiczba = 0f;
+            int _iOpcja = -1;
+
+            _fWynik = PodajLiczbeFloat();
 
             while (true)
             {
                 while (true)
                 {
-                    Console.WriteLine("0 - Wyjście z programu");
-                    Console.WriteLine("1 - Dodawanie");
-                    Console.WriteLine("2 - Odejmowanie");
-                    Console.WriteLine("3 - Mnożenie");
-                    Console.WriteLine("4 - Dzielenie");
-                    Console.Write("Wybierz jedna z opcji:");
+                    MenuOperacji();
 
                     if (int.TryParse(Console.ReadLine(), out _iOpcja) == true && _iOpcja >= 0 && _iOpcja <= 4)
                     {
